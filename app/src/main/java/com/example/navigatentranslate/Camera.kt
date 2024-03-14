df19f116740fba67da905d2fa0d27926e0222f70
ArtiGaund <artigaund2210@gmail.com>
2024-03-15 00:50:30 +0530

    Image is loading in app using camera and gallery as well

--- app/src/main/java/com/example/navigatentranslate/Camera.kt
+++ app/src/main/java/com/example/navigatentranslate/Camera.kt
@@ -1,20 +1,15 @@
 package com.example.navigatentranslate
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.github.dhaval2404.imagepicker.ImagePicker
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -22,9 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Camera : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
 
 
@@ -33,12 +26,9 @@ class Camera : Fragment() {
     private lateinit var cameraOpenId: Button
     lateinit var clickImageId: ImageView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
     }
 
@@ -50,52 +40,28 @@ class Camera : Fragment() {
         var view = inflater.inflate(R.layout.fragment_camera, container, false)
         //by id we can get each component which id is assigned in XML file
         //get buttons and imageView
-        cameraOpenId = view.findViewById(R.id.camera_id)
+        cameraOpenId = view.findViewById(R.id.button_camera)
         clickImageId = view.findViewById(R.id.click_image)
 
         //camera_open button is for open the camera and add the setOnClickListener
         //in this button
-        cameraOpenId.setOnClickListener(View.OnClickListener {
-            v: View ? ->
-            //create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            //start the activity with camera_intent, and request the pic_id
-            startActivityForResult(cameraIntent, pic_id)
-        })
+        cameraOpenId.setOnClickListener { v: View? ->
 
+
+            ImagePicker.with(this)
+                .compress(512)            //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(
+                    1080,
+                    1080
+                )    //Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
         return view
     }
 
     //this method will help to retrieve the image
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        //match the request 'pic id' with requestCode
-        if(requestCode == pic_id){
-            //BitMap is data structure of image file which store the image in memory
-            val photo = data!!.extras!!["data"] as Bitmap?
-            //set the image in imageView for display
-            clickImageId.setImageBitmap(photo)
+        clickImageId.setImageURI(data?.data)
         }
     }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Camera.
-         */
-        // TODO: Rename and change types and number of parameters
-        // Define the pic id
-        private const val pic_id: Int = 123
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Camera().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
